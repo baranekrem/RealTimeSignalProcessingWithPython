ff8fa33059027a332c213e1ab3e34022c15aa83a
Baran EKREM <baranekrem@outlook.com>
2019-10-20 03:56:36 +0300

    src

---
+++ Filter.py
@@ -0,0 +1,28 @@
+from scipy import arctan, pi
+
+class LowPassFilter(object):
+    def __init__(self, x, R, C, period):
+        self.__R = R
+        self.__C = C
+        self.__T = period
+        self.__x = x
+        self.__y = [0] * len(x)
+
+        self.__K1 = (self.__T / (self.__T + 2 * self.__R * self.__C))
+        self.__K2 = (self.__T / (self.__T + 2 * self.__R * self.__C))
+        self.__K3 = ((self.__T - 2 * self.__R * self.__C) / (self.__T + 2 * self.__R * self.__C))
+
+    def FilterApply(self):
+        for i in range(len(self.__x)):
+            self.__y[i] = (self.__x[i] * self.__K1) + (self.__x[i - 1] * self.__K2) - (self.__y[i - 1] * self.__K3)
+        return (self.__y)
+
+    def GetFrequency(self):
+        return (1 / (2 * pi * self.__R * self.__C))
+
+    def GetWarping(self):
+        return (2 / self.__T) * arctan(2 * pi * self.GetFrequency() * self.__T / 2) / (2 * pi)
+
+
+
+
