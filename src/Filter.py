f2e4b5cefb664fe54c22c8af1141d19d3ba30e60
Baran EKREM <baranekrem@outlook.com>
2019-10-24 23:43:15 +0300

    Update Filter.py

--- src/Filter.py
+++ src/Filter.py
@@ -5,16 +5,15 @@ class LowPassFilter(object):
         self.__R = R
         self.__C = C
         self.__T = period
-        self.__x = x
         self.__y = [0] * len(x)
 
         self.__K1 = (self.__T / (self.__T + 2 * self.__R * self.__C))
         self.__K2 = (self.__T / (self.__T + 2 * self.__R * self.__C))
         self.__K3 = ((self.__T - 2 * self.__R * self.__C) / (self.__T + 2 * self.__R * self.__C))
 
-    def FilterApply(self):
-        for i in range(len(self.__x)):
-            self.__y[i] = (self.__x[i] * self.__K1) + (self.__x[i - 1] * self.__K2) - (self.__y[i - 1] * self.__K3)
+    def FilterApply(self, __x):
+        for i in range(len(__x)):
+            self.__y[i] = (__x[i] * self.__K1) + (__x[i - 1] * self.__K2) - (self.__y[i - 1] * self.__K3)
         return (self.__y)
 
     def GetFrequency(self):
@@ -22,3 +21,4 @@ class LowPassFilter(object):
 
     def GetWarping(self):
         return (2 / self.__T) * arctan(2 * pi * self.GetFrequency() * self.__T / 2) / (2 * pi)
+
